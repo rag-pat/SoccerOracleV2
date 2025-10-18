568bce2b255c07b9929a44e0ec2843cc76092dd0
Rag <ragspatel2006@gmail.com>
2025-10-18 14:02:15 -0400

    cleanup

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,1 +1,2 @@
-api_key = "dc18282bb6ed1d22e542ca7434b4e10d"
+api_key = "a8b6ed58e1c1a72886abfeeafc597460"
+supabase_connection_uri = "postgresql://postgres:86FbLh4CAwbXk2gm@db.aktmpftpximkdnyjbzuq.supabase.co:5432/postgres"
