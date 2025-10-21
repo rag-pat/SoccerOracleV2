56e36d7806d51d0302019c080b596c7c9d7e6984
Rag <ragspatel2006@gmail.com>
2025-10-21 19:13:51 -0400

    feat: added very minor error handling

--- backend/app/api/routes/teams_routes.py
+++ backend/app/api/routes/teams_routes.py
@@ -7,4 +7,8 @@ teams_router = APIRouter()
 @teams_router.post("/team_id")
 def get_team(request: TeamSchema):
     result = get_team_id(request.team_name, request.league_name, request.season)
+
+    if not result:
+        return {"error": "Team not found."}
+
     return result
